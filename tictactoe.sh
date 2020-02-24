9b67df142cc6e875c73897139f016f244d2e40a2
abhiksrivastava <abhik.srivastava10003@gmail.com>
2020-02-24 14:17:05 +0530

    [ADDED:reset and display]

--- tictactoe.sh
+++ tictactoe.sh
@@ -4,7 +4,45 @@ echo "lets start the game "
 declare -A board
 
 check=$((RANDOM%2))
+TOTALCOUNT=9
 
+movecount=1
+
+
+function reset_board()
+{
+	for(( num=1; num<=3; num++ ))
+	do
+		for(( num_in=1; num_in<=3; num_in++ ))
+		do
+			board[$num,$num_in]="-"
+		done
+	done
+}
+function assign_symbol()
+{
+	#player=1
+	#computer=0
+	if [ $check -eq 1 ]
+	then
+		player="X"
+		computer="0"
+	else
+		player="0"
+		computer="X"
+	fi
+}
+function toss()
+{
+	#player=1
+	#computer=0
+	if [ $check -eq 1 ]
+	then
+		echo "Player has won the toss"
+	else
+		echo "Computer has won the toss"
+	fi
+}
 
 function display_board()
 {
