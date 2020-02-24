7be4e23f408346022b0c5aa0a48c5e861714390d
abhiksrivastava <abhik.srivastava10003@gmail.com>
2020-02-24 13:01:39 +0530

    [ADDED:displayboard]

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,10 +1,18 @@
 #tictac toe game
 echo "welcome to tictac toe game"
 echo "lets start the game "
-function roll()
-{
-   randomCheck=$((RANDOM%6 + 1))
-   echo "When we throw the die $randomCheck comes"
-   ((totalrandomCheck++))
-}
+declare -A board
 
+check=$((RANDOM%2))
+
+
+function display_board()
+{
+	for(( num=1; num<=3;  num++ ))
+	do
+		for(( num_in=1; num_in<=3; num_in++ ))
+		do
+			board[$num,$num_in]=""
+		done
+	done
+}
