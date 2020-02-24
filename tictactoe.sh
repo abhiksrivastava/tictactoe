c8c5352e26985481cee3c7d88e23cf3c832da6fc
abhiksrivastava <abhik.srivastava10003@gmail.com>
2020-02-24 12:49:42 +0530

    [ADDED:rollthedie]

--- tictactoe.sh
+++ tictactoe.sh
@@ -1,3 +1,10 @@
 #tictac toe game
 echo "welcome to tictac toe game"
 echo "lets start the game "
+function roll()
+{
+   randomCheck=$((RANDOM%6 + 1))
+   echo "When we throw the die $randomCheck comes"
+   ((totalrandomCheck++))
+}
+
